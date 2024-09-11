7d0e1610cb340df1bdd54517f5b2a2631145e9f4
a1exsDev <alexsvaldez2011@gmail.com>
2024-09-10 21:02:33 -0500

    first push

---
+++ indie-wiki-buddy.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Indie Wiki Buddy
+// @namespace    https://github.com/a1exsDev/indie-wiki-buddy-userscript
+// @version      1.0
+// @description  An alternative of the Indie Wiki Buddy extension, for people who can't install it. Supported wikis on Github repo
+// @author       a1exsDev
+// @require      ./fandoms.user.js
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/a1exsDev/indie-wiki-buddy-userscript/main/indie-wiki-buddy.js
+// @downloadURL  https://raw.githubusercontent.com/a1exsDev/indie-wiki-buddy-userscript/main/indie-wiki-buddy.js
+// @supportURL   https://github.com/a1exsDev/indie-wiki-buddy-userscript/issues/new/choose
+// @run-at       document-start
+// @license      Unlicense
+// ==/UserScript==
---
+++ userscripts/fandoms.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @match        https://*.fandom.com/*
+// @match        https://*.fextralife.com/*
+// @match        https://*.neoseeker.com/*
+// @match        https://breezewiki.com/*
+// @match        https://antifandom.com/*
+// @match        https://bw.artemislena.eu/*
+// @match        https://breezewiki.catsarch.com/*
+// @match        https://breezewiki.esmailelbob.xyz/*
+// @match        https://breezewiki.frontendfriendly.xyz/*
+// @match        https://bw.hamstro.dev/*
+// @match        https://breeze.hostux.net/*
+// @match        https://breezewiki.hyperreal.coffee/*
+// @match        https://breeze.mint.lgbt/*
+// @match        https://breezewiki.nadeko.net/*
+// @match        https://nerd.whatever.social/*
+// @match        https://breeze.nohost.network/*
+// @match        https://z.opnxng.com/*
+// @match        https://bw.projectsegfau.lt/*
+// @match        https://breezewiki.pussthecat.org/*
+// @match        https://bw.vern.cc/*
+// @match        https://breeze.whateveritworks.org/*
+// @match        https://breezewiki.woodland.cafe/*
+// ==/UserScript==
